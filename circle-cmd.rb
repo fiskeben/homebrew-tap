81d59bc08c3d92ac014fba82bd32e777bfc8cef2
Ricco Førgaard <ricco@fiskeben.dk>
2017-05-02 11:01:19 +0200

    Released 0.6.1

--- circle-cmd.rb
+++ circle-cmd.rb
@@ -1,8 +1,8 @@
 class CircleCmd < Formula
   desc "Use CircleCI from the command-line."
   homepage "https://github.com/fiskeben/circle-cmd"
-  url "https://github.com/fiskeben/circle-cmd/archive/0.6.0.tar.gz"
-  sha256 "21e0ab2812ce9619070bb25deb5800be3f083b513fb463f1de1db2fc124b7087"
+  url "https://github.com/fiskeben/circle-cmd/archive/0.6.1.tar.gz"
+  sha256 "d73fe519122f7a72b6ee9968e9d6b588cba176c23682dc06636964ce4abfb7e5"
 
   def install
     prefix.install Dir["*.rb"] - ["test.rb"]
