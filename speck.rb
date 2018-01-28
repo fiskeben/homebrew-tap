c044788aaf49a0029691a4a91957f17dff9515f0
Ricco Førgaard <ricco@fiskeben.dk>
2018-01-28 01:09:44 +0100

    Use alpha2

--- speck.rb
+++ speck.rb
@@ -1,10 +1,10 @@
  class Speck < Formula
   desc "micro.blog for the command line."
   homepage "https://github.com/fiskeben/speck"
-  url "https://github.com/fiskeben/speck/releases/download/1.0.0-alpha1/speck-1.0.0-alpha1.zip"
-  sha256 "79e32e6c8ad14bef73b81b9d6412f0c041a94ec5b370a5cae71d868488129365"
+  url "https://github.com/fiskeben/speck/releases/download/1.0.0-alpha2/speck-1.0.0-alpha2.zip"
+  sha256 "db19a0d4107b1d368aa7dff2d1cdd52f69cc27fac719ba7accdd95ce057e4bdd"
    def install
      prefix.install Dir["speck-*"]
-     bin.install_symlink prefix/'speck-1.0.0-alpha1' => "speck"
+     bin.install_symlink prefix/'speck-1.0.0-alpha2' => "speck"
    end
 end
